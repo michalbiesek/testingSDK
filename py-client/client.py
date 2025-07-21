51b09374e76394769be334073519b3b938f00de1
Michal Biesek <mbiesek@cribl.io>
2025-07-21 13:38:29 +0200

    Update client

--- py-client/client.py
+++ py-client/client.py
@@ -8,10 +8,15 @@ env_path = Path(__file__).parent.parent / ".env"
 load_dotenv(dotenv_path=env_path)
 
 if __name__ == "__main__":
+    audience = os.getenv("CRIBL_AUDIENCE")
+    if audience:
+        os.environ["CRIBLCONTROLPLANE_AUDIENCE"] = audience 
+
     workspace = os.getenv("WORKSPACE_NAME", "")
     org_id = os.getenv("ORG_ID", "")
     domain = os.getenv("CRIBL_DOMAIN", "")
     server_url = f"https://{workspace}-{org_id}.{domain}/api/v1"
+    wg_url = f"{server_url}/m/default"
 
     with CriblControlPlane(
         server_url=server_url,
@@ -26,3 +31,8 @@ if __name__ == "__main__":
 
         res = ccp_client.health.get_health_info()
         print(res)
+
+        inputs = ccp_client.inputs.list_input(server_url=wg_url)
+        if inputs.items:
+            for idx, item in enumerate(inputs.items):
+                print(f"Input {idx}  ID: {item.id}  Type: {item.type}")
